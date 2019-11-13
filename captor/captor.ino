f3030a0ad3d8591de9c1b10dfa4b90311a72b863
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-11-13 15:28:22 +0100

    Enhances and cleans the torsion script

--- captor/captor.ino
+++ captor/captor.ino
@@ -1,19 +1,30 @@
 int CAPTOR = A3;
-int LED = 2;
+int LED = 3; // Port on which the led is
 int THRESHOLD = 50;
 
-int torsion = 380;
+// the torsion values  must be calibrated beforehand
+int RAW_TORSION_MIN = 130;
+int RAW_TORSION_MAX = 460;
+int RAW_TORSION_MOY = 380;
+
+int torsion = RAW_TORSION_MOY;
 
 int getTorsion(int captor) {
   return analogRead(captor);
 }
 
 int reactTorsion(int led, int torsion) {
-  if (abs(torsion - 380) > THRESHOLD) {
+  if (abs(torsion - RAW_TORSION_MOY) > THRESHOLD) {
     digitalWrite(LED, HIGH);
   } else {
     digitalWrite(LED, LOW);
   }
+}
+
+// 130 - 380 - 460
+// 0   -  50 - 100
+int convertTorsion(int torsion) {
+  return map(torsion, RAW_TORSION_MIN, RAW_TORSION_MAX, 0, 255);
 }
 
 void setup() {
@@ -25,7 +36,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   torsion = getTorsion(CAPTOR);
-  Serial.println(torsion);
-  reactTorsion(LED, torsion);
+  Serial.print("Torsion: ");
+  Serial.print(torsion);
+  Serial.print(" - Mapped torsion: ");
+  Serial.println(convertTorsion(torsion));
+  analogWrite(LED, convertTorsion(torsion));
+  //reactTorsion(LED, Torsion);
   delay(100);
 }
