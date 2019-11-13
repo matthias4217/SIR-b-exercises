cb1d9a798ea4bb5d2b3f684e2827139d34dd906d
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-11-13 15:39:55 +0100

    More code cleaning

--- captor/captor.ino
+++ captor/captor.ino
@@ -7,12 +7,19 @@ int RAW_TORSION_MIN = 130;
 int RAW_TORSION_MAX = 460;
 int RAW_TORSION_MOY = 380;
 
+// we initialize a variable to keep the current torsion stored
 int torsion = RAW_TORSION_MOY;
 
 int getTorsion(int captor) {
   return analogRead(captor);
 }
 
+/**
+ * Set led on a digital pin
+ * If the difference between the current raw torsion and
+ * the average raw torsion is greater than THRESHOLD,
+ * the led is lit.
+ */
 int reactTorsion(int led, int torsion) {
   if (abs(torsion - RAW_TORSION_MOY) > THRESHOLD) {
     digitalWrite(LED, HIGH);
@@ -21,10 +28,16 @@ int reactTorsion(int led, int torsion) {
   }
 }
 
-// 130 - 380 - 460
-// 0   -  50 - 100
+
 int convertTorsion(int torsion) {
   return map(torsion, RAW_TORSION_MIN, RAW_TORSION_MAX, 0, 255);
+}
+
+void printTorsion(int torsion) {
+  Serial.print("Torsion: ");
+  Serial.print(torsion);
+  Serial.print(" - Mapped torsion: ");
+  Serial.println(convertTorsion(torsion));
 }
 
 void setup() {
@@ -36,10 +49,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   torsion = getTorsion(CAPTOR);
-  Serial.print("Torsion: ");
-  Serial.print(torsion);
-  Serial.print(" - Mapped torsion: ");
-  Serial.println(convertTorsion(torsion));
+  printTorsion(torsion);
   analogWrite(LED, convertTorsion(torsion));
   //reactTorsion(LED, Torsion);
   delay(100);
